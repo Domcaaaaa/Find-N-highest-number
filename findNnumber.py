7d1e3fdf7541a7b9221c84fb8e0ac4e4da4b0496
Dominik <97449498+Domcaaaaa@users.noreply.github.com>
2024-11-20 10:56:34 +0100

    Update findNnumber.py

--- findNnumber.py
+++ findNnumber.py
@@ -1,38 +1,37 @@
 import random
 
-def quickselect(arr, left, right, k):
-    if left == right:  
-        return arr[left]
+def quickselect_non_recursive(arr, left, right, k):
+    while left <= right:
+        pivot_index = random.randint(left, right)
+        pivot_value = arr[pivot_index]
 
-    pivot_index = random.randint(left, right)
-    pivot_value = arr[pivot_index]
+        arr[pivot_index], arr[right] = arr[right], arr[pivot_index]
+        
+        store_index = left
+        for i in range(left, right):
+            if arr[i] > pivot_value: 
+                arr[store_index], arr[i] = arr[i], arr[store_index]
+                store_index += 1
 
-    arr[pivot_index], arr[right] = arr[right], arr[pivot_index]
-    
-    store_index = left
-    for i in range(left, right):
-        if arr[i] > pivot_value:
-            arr[store_index], arr[i] = arr[i], arr[store_index]
-            store_index += 1
+        arr[store_index], arr[right] = arr[right], arr[store_index]
 
-    arr[store_index], arr[right] = arr[right], arr[store_index]
+        if k == store_index:
+            return arr[k]
+        elif k < store_index:
+            right = store_index - 1
+        else:
+            left = store_index + 1
 
-    if k == store_index:
-        return arr[k]
-    elif k < store_index:
-        return quickselect(arr, left, store_index - 1, k)
-    else:
-        return quickselect(arr, store_index + 1, right, k)
+    return arr[left]
 
 def find_number(numbers, which):
     numbers = [int(num) for num in numbers]
-    
     unique_numbers = list(set(numbers))
 
     if len(unique_numbers) < which:
         return None
     
-    return quickselect(unique_numbers, 0, len(unique_numbers) - 1, which - 1)
+    return quickselect_non_recursive(unique_numbers, 0, len(unique_numbers) - 1, which - 1)
 
 input_string = input('Enter your list of numbers (separated by space):\n') 
 which = int(input("Which position number do you want to find (from highest to lowest)?\n"))
